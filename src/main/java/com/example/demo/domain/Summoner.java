faa36bebe0ce4b8197e285f23ac285bc2ce0e279
joycjay <joycjay@163.com>
2019-04-23 18:15:19 +0200

    OPT: service IOP style; Real data from riot-api
    1.api-token in src/main/resources/application.properties

--- src/main/java/com/example/demo/domain/Summoner.java
+++ src/main/java/com/example/demo/domain/Summoner.java
@@ -4,39 +4,14 @@ import lombok.Data;
 
 @Data
 public class Summoner {
-	private String summonerId;
+	private String accountId;
 	private String name;
-	private int level;
+	private int summonerLevel;
+	private Long revisionDate;
 	
-	public Summoner(String summonerName) {
-		this.name = summonerName;
-	}
-	
+	// not very useful properties
+	private String puuid;
+	private int profileIconId;
+
 }
 
-
-/*
-public String getSummonerId() {
-	return summonerId;
-}
-
-public void setSummonerId(String summonerId) {
-	this.summonerId = summonerId;
-}
-
-public String getName() {
-	return name;
-}
-
-public void setName(String name) {
-	this.name = name;
-}
-
-public int getLevel() {
-	return level;
-}
-
-public void setLevel(int level) {
-	this.level = level;
-}
- */
